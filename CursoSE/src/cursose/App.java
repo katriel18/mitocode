c548c3716827bc82ee7802d50e94451563bb3a3a
Osti Katriel <katriel181920@example.com>
2021-02-24 17:51:44 -0500

    alcanze de las variables

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -2,21 +2,22 @@ package CursoSE.src.cursose;
 
 public class App {
 
-    private String nombre;
+    private String nombre="Maria";
 
-    public void saludar(String nombre){
+    public void saludar1(){
+        String nombre="Jose";
         System.out.println("Hola "+nombre);
     }
-    public String despedir(String nombre){
-        return nombre;
+    public void saludar2(){
+        System.out.println("Hola "+nombre);
     }
 
     public static void main(String[] args) throws Exception {
 
         App a=new App();
-        a.saludar("Katriel");
-        String mensaje=a.despedir("katriel");
-        System.out.println("Adios "+mensaje);
+        
+        a.saludar1();
+        a.saludar2();
     
     }
 }

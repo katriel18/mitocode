f529020c5e4f54c95f0bdfdae49c2931e0387efb
Osti Katriel <katriel181920@example.com>
2021-02-24 01:53:23 -0500

    tipos de datos

---
+++ CursoSE/src/cursose/App.java
@@ -0,0 +1,25 @@
+package CursoSE.src.cursose;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        int var1;
+        var1=1;
+        System.out.println(var1);
+      /*var1=null;  //no puede convertir
+        System.out.println(var1);*/
+
+        Integer var2;
+        var2=2;
+        System.out.println(var1);
+        var2=null;
+        System.out.println(var2);
+
+        double var3;
+        var3=3.3;
+        System.out.println(var3);
+        
+        String cadena="Hola Mundo";
+        System.out.println(cadena);
+    }
+}

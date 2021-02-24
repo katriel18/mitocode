bead27e8cb8b070ffcac44eb85ae5b2db504f415
Osti Katriel <katriel181920@example.com>
2021-02-24 16:36:27 -0500

    metodo burbuja con nombres

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -7,32 +7,33 @@ public class App {
         int cant = 5;
         Scanner sc = new Scanner(System.in);
 
-        // String[] nombres= new String[cant];
-        int[] edades = new int[cant];
+        String[] nombres = new String[cant];
+        // int[] edades = new int[cant];
 
         for (int i = 0; i < cant; i++) {
-            // System.out.println("Ingrese nombre " + i + ":");
-            // nombres[i] = sc.next();
-            System.out.println("Ingrese edad " + i + ":");
-            edades[i] = sc.nextInt();
+            System.out.println("Ingrese nombre " + i + ":");
+            nombres[i] = sc.next();
+            // System.out.println("Ingrese edad " + i + ":");
+            // edades[i] = sc.nextInt();
         }
 
         System.out.println("--------------------------");
 
-        int aux;
+        String aux;
 
         for (int i = 0; i < cant - 1; i++) {
             for (int j = 0; j < (cant - 1) - i; j++) {
-                if (edades[j] > edades[j + 1]) {
-                    aux = edades[j];
-                    edades[j] = edades[j + 1];
-                    edades[j + 1] = aux;
+                if (nombres[j].compareTo(nombres[j + 1]) > 0) {          //orden ascendente
+                    //if (nombres[j].compareTo(nombres[j + 1]) < 0) {   //orden descendente
+                    aux = nombres[j];
+                    nombres[j] = nombres[j + 1];
+                    nombres[j + 1] = aux;
                 }
 
             }
         }
 
-        for (Integer e : edades) {
+        for (String e : nombres) {
             System.out.println(e);
         }
 

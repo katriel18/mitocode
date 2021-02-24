5f0088d87edc04f1fa12b8d3f9141f64823f2928
Osti Katriel <katriel181920@example.com>
2021-02-24 17:44:45 -0500

    metodos

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -4,15 +4,19 @@ public class App {
 
     private String nombre;
 
-    public void saludar(){
-        nombre="Katriel";
+    public void saludar(String nombre){
         System.out.println("Hola "+nombre);
+    }
+    public String despedir(String nombre){
+        return nombre;
     }
 
     public static void main(String[] args) throws Exception {
 
         App a=new App();
-        a.saludar();
+        a.saludar("Katriel");
+        String mensaje=a.despedir("katriel");
+        System.out.println("Adios "+mensaje);
     
     }
 }

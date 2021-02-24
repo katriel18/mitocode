588dad65fdadd1dcf9d71265968c383cffb8e083
Osti Katriel <katriel181920@example.com>
2021-02-24 12:43:38 -0500

    ciclo do while

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -1,36 +1,17 @@
 package CursoSE.src.cursose;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for(int i=0;i<10;i=i+2){
-            System.out.println(i);
-        }
-        System.out.println("//////////////////");
-        for(int i=0;i<5;i++){
-            if (i==2) {
-                break;
-            }
-            System.out.println(i);
-        }
-        System.out.println("//////////////////");
-        for(int i=0;i<5;i++){
-            if (i==2) {
-                continue;
-            }
-            System.out.println(i);
-        }
-        System.out.println("//////////////////");
-        String[] array={"FACEBOOK","YOUTUBE","TWITER"};
-        for(int i=0;i<array.length;i++){
-            System.out.println(array[i]);
-        }
+        String opcion;
+        do{
+            System.out.println("Ingrese su nombre:");
+            Scanner sc=new Scanner(System.in);
+            opcion=sc.next();
 
-        System.out.println("//////////////////");
-        String[] array2={"FACEBOOK","YOUTUBE","TWITER"};
-        for(String s:array2){
-            System.out.println(s);
-        }
+        }while(opcion.equals("Osti"));
 
     }
 }

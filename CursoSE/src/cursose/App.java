9c1b8ebadf3d2ff23f473d138a8f0d8a3c8efe52
Osti Katriel <katriel181920@example.com>
2021-02-24 17:09:19 -0500

    switch

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -5,30 +5,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int cant = 3;
         Scanner sc = new Scanner(System.in);
 
-        int[][] numeros = new int[cant][cant];
+        String resultado = sc.next();
 
-        for (int i = 0; i < cant; i++) {
-            for (int j = 0; j < cant; j++) {
-                System.out.println("Ingrese numero " + i + "," + j + ": ");
-                numeros[i][j] = sc.nextInt();
-            }
+        switch (resultado) {
+            case "Osti":
+                System.out.println("Hola Osti");
+                break;
+            case "Katriel":
+                System.out.println("Hola Katriel");
+                break;
+            default:
+                System.out.println("No encontrado");
+                break;
         }
-
-        System.out.println("-------------------");
-
-        for (int i = 0; i < cant; i++) {
-            for (int j = 0; j < cant; j++) {
-                    if(i==j){
-                        System.out.print(numeros[i][j]);
-                    }else{
-                        System.out.print(0);
-                    }
-            }
-            System.out.println();
-        }
+        System.out.println("Fin del Switch");
 
     }
 }

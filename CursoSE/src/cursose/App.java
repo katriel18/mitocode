d0a5c7334fc564d1bfb2853762e4b0c2d6c1d679
Osti Katriel <katriel181920@example.com>
2021-02-25 00:10:39 -0500

    manejo de fechas

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -1,17 +1,24 @@
 package CursoSE.src.cursose;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class App {
 
     public static void main(String[] args) {
 
-        Persona p=new Persona();
+        Date fecha =new Date();
+        System.out.println(fecha.getYear()+1900);//año
+        System.out.println(fecha.getDate());    //dia
 
-        p.setNombre("katriel");
-        p.setEdad(22);
+        System.out.println("-------------");
+        Calendar calendario =Calendar.getInstance();
+        System.out.println(calendario.get(Calendar.YEAR));  
+        System.out.println(calendario.get(Calendar.DAY_OF_MONTH)); 
 
-        p.mostrarInstancia();//metodo del objeto
-
-        Persona.mostrarClase();//metodo de la clase
-
+        System.out.println("-------------");
+        Calendar calendario2 =new GregorianCalendar();
+        System.out.println(calendario2.get(Calendar.YEAR)); 
     }
 }

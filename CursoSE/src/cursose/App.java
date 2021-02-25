2afd5373d7f8a9daa7d1b034219f2b25c0762443
Osti Katriel <katriel181920@example.com>
2021-02-25 13:35:56 -0500

    HashMap

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -1,68 +1,24 @@
 package CursoSE.src.cursose;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class App {
 
     public static void main(String[] args) {
 
-        //ArryList = get
-        //Linkedlist= add / remove
+        Map<String,String> diccionario =new HashMap<>();
+
+        diccionario.put("uno", "1");
+        diccionario.put("dos", "2");
+        diccionario.put("tres", "3");
+
+        String contenido=diccionario.get("tres");
         
-        System.out.println("ADD:");
-        System.out.println("LinkedList:");
-        Long i1 = System.nanoTime();
-        List<Integer> LinkedList = new LinkedList<>();
-        for (int i = 0; i < 10000; i++) {
-            LinkedList.add(i);//+
-        }
-        Long f1 = System.nanoTime();
-        System.out.println(f1 - i1);
-
-        System.out.println("ArrayList:");
-        Long i2 = System.nanoTime();
-        List<Integer> ArrayList = new ArrayList<>();
-        for (int i = 0; i < 10000; i++) {
-            ArrayList.add(i);//-
-        }
-        Long f2 = System.nanoTime();
-        System.out.println(f2 - i2);
-        //////////////////////////////////////////////
-        System.out.println("GET:");
-        System.out.println("LinkedList:");
-        i1 = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            LinkedList.get(i);//-
-        }
-        f1 = System.nanoTime();
-        System.out.println(f1 - i1);
-
-        System.out.println("ArrayList:");
-        i2 = System.nanoTime();
-        for (int i = 0; i < 10000; i++) {
-            ArrayList.get(i);//+
-        }
-        f2 = System.nanoTime();
-        System.out.println(f2 - i2);
-        //////////////////////////////////////////////
-        System.out.println("REMOVE:");
-        System.out.println("LinkedList:");
-        i1 = System.nanoTime();
-        for (int i = 9999; i >=0; i--) {
-            LinkedList.remove(i);//+
-        }
-        f1 = System.nanoTime();
-        System.out.println(f1 - i1);
-
-        System.out.println("ArrayList:");
-        i2 = System.nanoTime();
-        for (int i = 9999; i >=0; i--) {
-            ArrayList.remove(i);//-
-        }
-        f2 = System.nanoTime();
-        System.out.println(f2 - i2);
+        System.out.println(contenido);
 
     }
 }

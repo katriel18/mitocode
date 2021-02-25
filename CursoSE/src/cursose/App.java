33734ce4d6e187e7144abdede37c58a48352566e
Osti Katriel <katriel181920@example.com>
2021-02-25 12:04:47 -0500

    ArrayList y eficiencia

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -9,19 +9,30 @@ public class App {
     public static void main(String[] args) {
 
    
-        List<String> lista =new ArrayList<>();
+        Long i1=System.nanoTime();
+        List<Integer> lista =new ArrayList<>();
 
-        lista.add("Hola");
-        lista.add("Mundo");
+        lista.add(1); //1 procesador 1 bloque
+        lista.add(2); //1 procesador 1 bloque
+        lista.add(3); //1 procesador 1 bloque
+        lista.add(4); //1 procesador 1 bloque
+        lista.add(5); //1 procesador 1 bloque
+        Long f1=System.nanoTime();
+        System.out.println(f1-i1);//[38001-41400]
+
+        /////////////////////////////////////////
         
-        for (String t : lista) {
-            System.out.println(t);
-        }
+        Long i2=System.nanoTime();
+        List<Integer> lista2 =new ArrayList<>(5);
 
-        //Objeto Iterator, permite recorrer sobre una lista
-        Iterator<String> i=lista.iterator();
-        while (i.hasNext()) {
-            System.out.println(i.next());
-        }
+        //1 procesador 5 bloques
+        lista2.add(1); //a cada bloque
+        lista2.add(2); //a cada bloque
+        lista2.add(3); //a cada bloque
+        lista2.add(4); //a cada bloque
+        lista2.add(5); //a cada bloque
+        Long f2=System.nanoTime();
+        System.out.println(f2-i2);//[4100-4900]
+
     }
 }

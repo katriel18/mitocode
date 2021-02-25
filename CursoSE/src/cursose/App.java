b9f2dbbe18b0b0a9000e9d4a4acc5fa6c798bf0f
Osti Katriel <katriel181920@example.com>
2021-02-25 11:54:40 -0500

    colecciones y iterator

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -1,33 +1,27 @@
 package CursoSE.src.cursose;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
-        ////////////////////////////Hace uso de 1000 objetos en la concatenacion
-       
-        long t_i=System.nanoTime();
+   
+        List<String> lista =new ArrayList<>();
+
+        lista.add("Hola");
+        lista.add("Mundo");
         
-        String texto="";
-        for (int i = 0; i < 1000; i++) {
-            texto+=i;
+        for (String t : lista) {
+            System.out.println(t);
         }
 
-        long t_f=System.nanoTime();
-        double diferencia=t_f-t_i;
-        System.out.println("String:\n"+diferencia);
-       
-        ///////////////////////////////Hace uso de un solo objeto en la concatenacion - eficiente 
-        
-        long t_i2=System.nanoTime();
-        
-        StringBuilder builder=new StringBuilder();
-        for (int i = 0; i < 1000; i++) {
-            builder.append(i);
+        //Objeto Iterator, permite recorrer sobre una lista
+        Iterator<String> i=lista.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
         }
-
-        long t_f2=System.nanoTime();
-        double diferencia2=t_f2-t_i2;
-        System.out.println("StringBuilder:\n"+diferencia2);
     }
 }

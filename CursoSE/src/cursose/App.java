b43e4e8f01810579717c22fff598d7b57f396823
Osti Katriel <katriel181920@example.com>
2021-02-25 12:32:59 -0500

    LinkedList

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -1,7 +1,6 @@
 package CursoSE.src.cursose;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class App {
@@ -9,30 +8,14 @@ public class App {
     public static void main(String[] args) {
 
    
-        Long i1=System.nanoTime();
-        List<Integer> lista =new ArrayList<>();
+        List<Integer> lista =new LinkedList<>();
 
-        lista.add(1); //1 procesador 1 bloque
-        lista.add(2); //1 procesador 1 bloque
-        lista.add(3); //1 procesador 1 bloque
-        lista.add(4); //1 procesador 1 bloque
-        lista.add(5); //1 procesador 1 bloque
-        Long f1=System.nanoTime();
-        System.out.println(f1-i1);//[38001-41400]
+        lista.add(11);//indice 0
+        lista.add(22);//indice 1
+        lista.add(33);//indice 2
 
-        /////////////////////////////////////////
-        
-        Long i2=System.nanoTime();
-        List<Integer> lista2 =new ArrayList<>(5);
-
-        //1 procesador 5 bloques
-        lista2.add(1); //a cada bloque
-        lista2.add(2); //a cada bloque
-        lista2.add(3); //a cada bloque
-        lista2.add(4); //a cada bloque
-        lista2.add(5); //a cada bloque
-        Long f2=System.nanoTime();
-        System.out.println(f2-i2);//[4100-4900]
+        System.out.println("Size: "+lista.size());
+        System.out.println(lista.get(1));
 
     }
 }

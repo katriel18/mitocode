3de003434a3dd220bce56595c65928819b18a8a6
Osti Katriel <katriel181920@example.com>
2021-02-25 19:28:54 -0500

    Excepcion personalizada 2

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExcepcionPersonalizada {
 
         final int EDAD_MAXIMA = 100;
         Scanner sc = new Scanner(System.in);
@@ -15,14 +15,10 @@ public class App {
         if (edad < EDAD_MAXIMA) {
             System.out.println("Bienvenido");
         } else {
-            try {
-                throw new ExcepcionPersonalizada("Edad no permitida");
-            } catch (ExcepcionPersonalizada e) {
-               // e.printStackTrace();
-                System.out.println(e.getMessage());
-            }
-        }
 
+            throw new ExcepcionPersonalizada("Edad no permitida");
+
+        }
 
     }
 }

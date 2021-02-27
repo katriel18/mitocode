5847bccf7ba86d7c4d533ba14fef45ff6211e4d1
Osti Katriel <katriel181920@example.com>
2021-02-26 21:15:07 -0500

    Conexion a base de datos local (estable)

--- CursoSE/src/cursose/App.java
+++ CursoSE/src/cursose/App.java
@@ -9,43 +9,37 @@ public class App {
 
     public static void main(String[] args) {
 
-        // JDBC driver y base de datos URL
-        // final String JDBC_DRIVER = "org.postgresql.Driver";// com.mysql.jdbc.Driver
-        final String DB_URL = "jdbc:postgresql://localhost:5432/mitocode";// jdbc:mysql://localhost/mitocode
-        // credenciales
+        final String DB_URL = "jdbc:postgresql://localhost:5432/mitocode";
+
         final String USER = "postgres";
         final String PASS = "postgres";
 
         Connection conexion = null;
 
         try {
-            // Class.forName(JDBC_DRIVER);
+
             conexion = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("BD conectada!");
-           //PreparedStatement st = conexion.prepareStatement("INSERT INTO persona (nombre) VALUES ('maria')");
-            PreparedStatement st = conexion.prepareStatement("INSERT INTO usuario VALUES ('')");
+            PreparedStatement st = conexion.prepareStatement("INSERT INTO persona(nombre) VALUES ('Juan')");
             st.executeUpdate();
             st.close();
-            /*
-             * } catch (ClassNotFoundException e) {
-             * 
-             * System.out.println("Exepcion 1 ejecutada!"); e.printStackTrace();
-             */
+
         } catch (SQLException e) {
 
-            System.out.println("Exepcion 2 ejecutada!");
-            e.printStackTrace();
+            System.out.println("Exepcion 1 ejecutada: " + e.getMessage());
 
         } finally {
 
             if (conexion != null) {
                 try {
-                    if (!conexion.isClosed()) {
+
+                    if (!conexion.isClosed())
                         conexion.close();
-                    }
+
                 } catch (SQLException e) {
-                    System.out.println("Exepcion 3 ejecutada!");
-                    e.printStackTrace();
+
+                    System.out.println("Exepcion 2 ejecutada: " + e.getMessage());
+
                 }
             }
 

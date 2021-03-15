aa11d56c64044e6c3b139b507d95d367398cd120
Osti Katriel <katriel181920@example.com>
2021-03-15 12:55:07 -0500

    NIO2 Channel y Buffer

--- CursoSE/src/main/java/osti/katriel/tema18/AppNIO.java
+++ CursoSE/src/main/java/osti/katriel/tema18/AppNIO.java
@@ -1,6 +1,9 @@
 package osti.katriel.tema18;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -57,6 +60,48 @@ public class AppNIO {
         String texto="Suscribanse";
         Files.write(path, texto.getBytes());
     }
+
+    public void leerPorChannel() throws IOException {
+		RandomAccessFile file = new RandomAccessFile("mitocodeCopy.txt", "rw");
+		FileChannel channel = file.getChannel();
+		
+		long size = channel.size();
+		ByteBuffer buf = ByteBuffer.allocate((int)size);
+
+		int bytesRead = channel.read(buf);
+		while (bytesRead != -1) {
+
+			System.out.println("Leidos " + bytesRead);
+			
+			buf.flip(); //Preparado el buffer para leer
+
+			while (buf.hasRemaining()) {
+				System.out.print((char) buf.get());
+			}
+
+			buf.clear(); //Cambio a modo escritura
+			bytesRead = channel.read(buf);
+		}
+		file.close();
+	}
+
+	public void escribirPorChannel() throws IOException {
+		RandomAccessFile file = new RandomAccessFile("mitocodeCopy.txt", "rw");
+		FileChannel channel = file.getChannel();
+		
+		String texto = "Hola";		
+
+		ByteBuffer buf = ByteBuffer.allocate(128);
+		buf.clear(); //modo escritura
+		buf.put(texto.getBytes());
+
+		buf.flip();
+
+		while(buf.hasRemaining()) {
+		    channel.write(buf);
+		}
+		file.close();
+	}
 
     public static void main(String args[]) throws IOException {
         AppNIO app=new AppNIO();

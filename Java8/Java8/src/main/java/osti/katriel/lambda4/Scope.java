f8327108aba3e8951a5f2ba35f89bf31f27db798
Osti Katriel <katriel181920@example.com>
2021-03-16 16:55:42 -0500

    variables globales

--- Java8/Java8/src/main/java/osti/katriel/lambda4/Scope.java
+++ Java8/Java8/src/main/java/osti/katriel/lambda4/Scope.java
@@ -2,7 +2,10 @@ package osti.katriel.lambda4;
 
 public class Scope {
 
-    public void probarScope() {
+    private static double atributo1;
+    private Double atributo2;
+
+    public void probarScopeLocal() {
         
        double n3=10;// Es final por defecto //Ya no se puede modificar
 
@@ -26,10 +29,39 @@ public class Scope {
 
     }
 
+    public void probarScopeStatic(){
+
+        //jdk<1.7
+        Operacion operacion=new Operacion(){
+
+            @Override
+            public double calcular(double n1, double n2) {
+                atributo1=n1+n2;
+                atributo2=atributo1+10;
+                return atributo2;
+            }
+            
+        };
+        System.out.println(operacion.calcular(5, 5));
+
+        //JDK>1.8
+        Operacion operacion2=(double n1, double n2)->{
+            atributo1=n1+n2;
+            atributo2=atributo1+10;
+            return atributo2;
+        };
+
+        System.out.println(operacion2.calcular(5, 5));
+
+    }
+
+
     public static void main(String args[]) {
        
         Scope s=new Scope();
-        s.probarScope();
-        
+        //s.probarScopeLocal();
+        s.probarScopeStatic();
+
     }
+    
 }
